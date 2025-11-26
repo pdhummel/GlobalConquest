505cafe316c61c8fb5128e88957504369817abba
Paul Hummel <pd_hummel@yahoo.com>
2025-11-26 10:46:11 -0800

    bump version

--- GameState.cs
+++ GameState.cs
@@ -32,7 +32,7 @@ public class GameState
 
 
     // if any of the data elements in the entities change above, then this version should be bumped.
-    public string Version { get; set; } = "v0.5.0";
+    public string Version { get; set; } = "v0.5.1";
     private Random rand = new System.Random();
 
 
